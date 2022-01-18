7c5f30365c93a3db831cc5263e390fdfe36dcd04
mohamedommran <mohamed.ommran@gmail.com>
2022-01-18 12:16:06 +0100

    first commit of the project

---
+++ products.js
@@ -0,0 +1,63 @@
+//PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Make up 1",
+    desc : "Greatest properly off ham exercise all.",
+    img : "product1.png",
+    price : 120,
+    category : "makeup"
+  },
+  124: {
+    name : "Make up 2",
+    desc : "Unsatiable its possession nor off.",
+    img : "product2.png",
+    price : 120,
+    category : "makeup"
+
+  },
+  125: {
+    name : "Make up 3",
+    desc : "All difficulty unreserved the solicitude.",
+    img : "product3.png",
+    price : 120,
+    category : "makeup"
+
+  },
+  126: {
+    name : "Make up 4",
+    desc : "Had judgment out property the supplied. ",
+    img : "product4.png",
+    price : 130,
+    category : "skinCare"
+
+  },
+  127: {
+    name : "Make up 5",
+    desc : "Greatest properly off ham exercise all.",
+    img : "product5.png",
+    price : 120,
+    category : "skinCare"
+  },
+  128: {
+    name : "Make up 6",
+    desc : "Unsatiable its possession nor off.",
+    img : "product6.png",
+    price : 120,
+    category : "skinCare"
+  },
+  129: {
+    name : "Make up 7",
+    desc : "All difficulty unreserved the solicitude.",
+    img : "product7.png",
+    price : 120,
+    category : "accessoires"
+  },
+  130: {
+    name : "Make up 8",
+    desc : "Had judgment out property the supplied. ",
+    img : "product8.png",
+    price : 130,
+    category : "accessoires"
+
+  }
+};
